8cab22d2462573d3c5d66b5e1f93825f480837b2
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-09 16:11:07 +0100

    started the project and added jsx code

--- JSM_portfolio/frontend_react/src/App.js
+++ JSM_portfolio/frontend_react/src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 
 import {About, Footer, Header, Skills, Testimonials, Works } from './Containers'
+import './App.scss'
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className='app'>
         <Navbar/>
         <Header/>
         <About/>
